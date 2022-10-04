d88f94bffcac694db26b99ca4ef121f36397e3b2
Flanker1975 <bgabor91@gmail.com>
2022-10-04 12:47:49 +0200

    added hiding api

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,14 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBulx8jo4HwfS9eUPgYj5jnPvPQZtuOu-o',
-  authDomain: 'hru-referees.firebaseapp.com',
-  projectId: 'hru-referees',
-  storageBucket: 'hru-referees.appspot.com',
-  messagingSenderId: '386421740974',
-  appId: '1:386421740974:web:8052e22c730f50f563643f',
-  measurementId: 'G-8S5JGTHPRG',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAI,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
 
 const app = initializeApp(firebaseConfig)
